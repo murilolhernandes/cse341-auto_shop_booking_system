186d41024ee96f911bf84261adba849759a891ec
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2026-02-02 14:17:17 -0700

    added authentication through Google OAuth2.0

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,17 @@ router.use('/', require('./swagger'));
 router.get('/', (req, res) => {
   //#swagger.tags=['Index Page']
   // Remove this line and uncomment the line bellow once authentication is up and running.
-  // res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged out");
-  res.send("Welcome to the Auto Shop Booking System API").json;
+  res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged out");
+  // res.send("Welcome to the Auto Shop Booking System API").json;
 })
+
+router.get('/login', passport.authenticate('google', { scope: ['profile', 'email'] }), (req, res) => {});
+
+router.get('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
 
 module.exports = router;
